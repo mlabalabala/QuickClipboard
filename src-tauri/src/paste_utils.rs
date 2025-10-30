332037be16c6edfb11136d1b915fc5090fc77738
1842047134@qq.com <91363385+mlabalabala@users.noreply.github.com>
2025-10-30 10:21:20 +0800

    Update paste_utils.rs

--- src-tauri/src/paste_utils.rs
+++ src-tauri/src/paste_utils.rs
@@ -2,14 +2,16 @@
 
 #[cfg(windows)]
 pub fn windows_paste() -> bool {
+    // 引入了 VK_SHIFT 和 VK_INSERT
     use windows::Win32::UI::Input::KeyboardAndMouse::{
         SendInput, INPUT, INPUT_KEYBOARD, KEYBDINPUT, KEYBD_EVENT_FLAGS,
-        KEYEVENTF_KEYUP, VK_CONTROL, VK_V, VK_MENU,
+        KEYEVENTF_KEYUP, VK_SHIFT, VK_INSERT, VK_MENU,
     };
 
     unsafe {
         // 从 input_monitor 获取当前按键状态
-        let (ctrl_pressed, alt_pressed, _, _) = crate::input_monitor::get_modifier_keys_state();
+        // 假设 get_modifier_keys_state 返回 (ctrl, alt, shift, win)
+        let (_, alt_pressed, shift_pressed, _) = crate::input_monitor::get_modifier_keys_state();
 
         // 生成按键序列
         let mut inputs: Vec<INPUT> = Vec::new();
@@ -28,56 +30,56 @@ pub fn windows_paste() -> bool {
             inputs.push(alt_up);
         }
 
-        if !ctrl_pressed {
-            // 按下 Ctrl
-            let mut ctrl_down = INPUT::default();
-            ctrl_down.r#type = INPUT_KEYBOARD;
-            ctrl_down.Anonymous.ki = KEYBDINPUT {
-                wVk: VK_CONTROL,
+        // 如果 Shift 键没有被按下，则模拟按下 Shift
+        if !shift_pressed {
+            let mut shift_down = INPUT::default();
+            shift_down.r#type = INPUT_KEYBOARD;
+            shift_down.Anonymous.ki = KEYBDINPUT {
+                wVk: VK_SHIFT, // 使用 VK_SHIFT
                 wScan: 0,
                 dwFlags: KEYBD_EVENT_FLAGS(0),
                 time: 0,
                 dwExtraInfo: 0,
             };
-            inputs.push(ctrl_down);
+            inputs.push(shift_down);
         }
 
-        // 按下 V
-        let mut v_down = INPUT::default();
-        v_down.r#type = INPUT_KEYBOARD;
-        v_down.Anonymous.ki = KEYBDINPUT {
-            wVk: VK_V,
+        // 按下 Insert
+        let mut insert_down = INPUT::default();
+        insert_down.r#type = INPUT_KEYBOARD;
+        insert_down.Anonymous.ki = KEYBDINPUT {
+            wVk: VK_INSERT, // 使用 VK_INSERT
             wScan: 0,
             dwFlags: KEYBD_EVENT_FLAGS(0),
             time: 0,
             dwExtraInfo: 0,
         };
-        inputs.push(v_down);
+        inputs.push(insert_down);
 
-        // 释放 V
-        let mut v_up = INPUT::default();
-        v_up.r#type = INPUT_KEYBOARD;
-        v_up.Anonymous.ki = KEYBDINPUT {
-            wVk: VK_V,
+        // 释放 Insert
+        let mut insert_up = INPUT::default();
+        insert_up.r#type = INPUT_KEYBOARD;
+        insert_up.Anonymous.ki = KEYBDINPUT {
+            wVk: VK_INSERT, // 使用 VK_INSERT
             wScan: 0,
             dwFlags: KEYEVENTF_KEYUP,
             time: 0,
             dwExtraInfo: 0,
         };
-        inputs.push(v_up);
+        inputs.push(insert_up);
 
-        if !ctrl_pressed {
-            // 释放 Ctrl
-            let mut ctrl_up = INPUT::default();
-            ctrl_up.r#type = INPUT_KEYBOARD;
-            ctrl_up.Anonymous.ki = KEYBDINPUT {
-                wVk: VK_CONTROL,
+        // 如果 Shift 键之前不是由我们按下的，则模拟释放 Shift
+        if !shift_pressed {
+            let mut shift_up = INPUT::default();
+            shift_up.r#type = INPUT_KEYBOARD;
+            shift_up.Anonymous.ki = KEYBDINPUT {
+                wVk: VK_SHIFT, // 使用 VK_SHIFT
                 wScan: 0,
                 dwFlags: KEYEVENTF_KEYUP,
                 time: 0,
                 dwExtraInfo: 0,
             };
-            inputs.push(ctrl_up);
+            inputs.push(shift_up);
         }
 
         // 如果Alt键之前被按下，恢复它的状态
